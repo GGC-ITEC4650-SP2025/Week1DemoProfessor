83c8e64554be788b3a0fabe451f85d8277fce506
Sebastien Siva <ssiva@ggc.edu>
2025-01-07 22:22:39 +0000

    Lecture 1_7_25

--- DemoCSharpProj/ConsoleApp1/Program.cs
+++ DemoCSharpProj/ConsoleApp1/Program.cs
@@ -4,8 +4,31 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Fruit f = new Fruit("Pear", 1.05);
-        Console.WriteLine(f);
+        /*
+        for(int i = 0; i < 10; i++) {
+            Console.WriteLine("Hello World!");
+        }
+        */
+
+        /*
+        Console.Write("What's your name? ");
+        String a = Console.ReadLine();
+        Console.WriteLine(a + " is a fake name. You mus think I am a bot. I promise I am real.");
+        */
+        float x = 3.0f;
+
+        Random rgen = new Random();
+        for(int i = 0; i < 100; i++) {
+            //Console.WriteLine(rgen.Next(1, 4));
+        }
+
+        int[] nums = new int[5];
+        nums[2] = 121342;
+        for(int i = 0; i < 5; i++) {
+            Console.WriteLine(nums[i]);
+        }
+
+
+
     }
 }
