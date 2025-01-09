e60df4a16eac135809c064de1542663f47725b3b
Sebastien Siva <ssiva@ggc.edu>
2025-01-09 21:39:46 +0000

    Lecture 1_9_25

--- DemoCSharpProj/ConsoleApp1/Program.cs
+++ DemoCSharpProj/ConsoleApp1/Program.cs
@@ -2,6 +2,17 @@
 
 public class Program
 {
+    static void printBoard(String[,] b) {
+        Console.WriteLine("*****************");
+        for(int y = 0; y < 5; y++) {
+            for(int x = 0; x < 5; x++) {
+                Console.Write(b[x, y] + " ");
+            }
+            Console.Write("\n");
+        }
+        Console.WriteLine("*****************");
+    }
+
     static void Main(string[] args)
     {
         /*
@@ -15,7 +26,7 @@ public class Program
         String a = Console.ReadLine();
         Console.WriteLine(a + " is a fake name. You mus think I am a bot. I promise I am real.");
         */
-        float x = 3.0f;
+        //float x = 3.0f;
 
 
         Random rgen = new Random();
@@ -32,17 +43,40 @@ public class Program
             Console.WriteLine("Tails");
         }
 
-        /*
         int[] nums = new int[5];
         nums[2] = 121342;
         for(int i = 0; i < 5; i++) {
-            Console.WriteLine(nums[i]);
+            //Console.WriteLine(nums[i]);
         }
-        */
+        
+        //2D Array of strings
+        String[,] board = new String[5,5];
+        for(int y = 0; y < 5; y++) {
+            for(int x = 0; x < 5; x++) {
+                board[x, y] = ".";
+            }
+        }
+        //board[0, 0] = "A";
+        //board[1, 0] = "B";
+        printBoard(board);
 
-        // Example Change.
+        Point playerPos = new Point(0, 0);
+        board[playerPos.x, playerPos.y] = "P";
+        printBoard(board);
+        Point up = new Point(0, -1);
+        Point down = new Point(0, 1);
+        Point right = new Point(1, 0);
+        Point left = new Point(-1, 0);
 
+        //move player 
+        board[playerPos.x, playerPos.y] = "."; //erase old
+        Point temp = playerPos.add(up); //change pos
+        if(board[temp.x, temp.y] == "*") {
 
-
+        }
+        else {
+            playerPos = temp;
+        }
+        board[playerPos.x, playerPos.y] = "P"; //write new
     }
 }
